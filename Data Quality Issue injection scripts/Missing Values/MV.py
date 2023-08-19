fe08f5132bed9c21a41d15cf9b8cd6b935a1b590
BeckersNoah <142595665+BeckersNoah@users.noreply.github.com>
2023-08-19 10:28:56 +0200

    Add files via upload

--- Data Quality Issue injection scripts/Missing Values/MV.py
+++ Data Quality Issue injection scripts/Missing Values/MV.py
@@ -1,1 +1,23 @@
+import os
+import random
 
+#Set path to original sensor log file
+dr = 'C:/xxx/Sensor Log 100 records.txt'
+lines = 0
+
+#Open the file
+with open(dr, "r") as datafile:
+    #Write the new log with missing values to this file
+    with open("MV.txt","w") as test:
+        for line in datafile:
+            words = line.split()
+            l = " ".join([words[0],words[1],words[2],words[3]])
+            # 10% chance the following statement is true, when this happens the line doesnt get written down and the counter increases by 1
+            if random.randint(0,9) == 0:
+                lines += 1
+                continue
+            else:
+                test.write(l + '\n')
+
+#Print the number of skipping lines to the log
+print(lines)
